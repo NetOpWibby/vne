63e638379340632e8c6d39c67934218a7332b849
ポール ウェッブ <netopwibby@thenetwork.email>
2018-09-24 17:03:37 -0500

    Fixed a major issue

--- index.js
+++ index.js
@@ -24,12 +24,18 @@ for (const variable in _env) {
   const cleanVariable = variable.replace(/\d+/g, ""); // remove numbers from variable name
 
   switch(true) {
-    case (variable.match(numberedStringRegex)):
+    case (
+      variable.match(numberedStringRegex) &&
+      variable.match(numberedStringRegex).length > 0
+    ):
       if (!defaultObject[cleanVariable]) defaultObject[cleanVariable] = []; // if array does not exist, create it
       defaultObject[cleanVariable].push(_env[variable]);
       break;
 
-    case (variable.match(separatorRegex)):
+    case (
+      variable.match(separatorRegex) &&
+      variable.match(separatorRegex).length > 0
+    ):
       nestedObject[variable.split(separatorRegex)[0]] = nestedObject[variable.split(separatorRegex)[0]] || {};
       nestedObject[variable.split(separatorRegex)[0]][variable.split(separatorRegex)[1]] = _env[variable];
       break;
